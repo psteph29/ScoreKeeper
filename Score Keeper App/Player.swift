b2f0831fbb1015bf0b84e8eb4c40eb8c77422d00
Paige Stephenson <paigestephenson29@gmail.com>
2023-04-11 09:17:19 -0600

    first commit

---
+++ Score Keeper App/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  Score Keeper App
+//
+//  Created by Paige Stephenson on 4/8/23.
+//
+
+import Foundation
---
+++ Score Keeper App/PlayerScoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerScoreTableViewCell.swift
+//  Score Keeper App
+//
+//  Created by Paige Stephenson on 3/30/23.
+//
+
+import UIKit
+
+class PlayerScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
